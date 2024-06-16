4f998e12c98a6fc515644a950c8b5f74205d1bce
BrendanGCollins <163342759+BrendanGCollins@users.noreply.github.com>
2024-06-16 23:25:11 +0000

    Create btns for cells and add callback function for revealing and flagging cells

--- run.py
+++ run.py
@@ -1,29 +1,30 @@
-#Import tkinter library/random number generator and messagebox
+#Import tkinter library, random number generator and messagebox
 from tkinter import *
 import random
 from tkinter import messagebox
 
+#Create main window
 root = Tk()
-#Background Color
+#Set background Color
 root.configure(bg = "blue")
-#Set game area size
+#Set size of main window
 root.geometry("600x600")
 #Add game name
 root.title("Minesweeper Game")
 #Prevent game area changing outside given height x width
 root.resizable(False, False)
 
-#Add top frame dimensions and color
+#Add top frame for start button
 top_frame = Frame(
     root, bg = "grey",
     width = 600,
     height = 120,
 )
 
-#Add top frame position
+#Position frome at the top
 top_frame.place(x=0, y=0)
 
-#Add right frame dimensions and color
+#Add right frame for remaining mines
 right_frame = Frame(
     root, bg = "grey",
     width = 120,
@@ -40,7 +41,7 @@ game_area = Frame(
     height = 480,
 )
 
-#Add game frame position
+#Position the game area frame below the top frame and to the left of the right frame
 game_area.place(x=0, y=120)
 
 class Minesweeper:
@@ -77,11 +78,38 @@ class Minesweeper:
         #Set to hold mine locations
         self.mines_set = set()
 
-        #Place mines on board manually
+        #Place mines on board
         while len(self.mines_set) < self.mines:
             row = random.randint(0, self.rows -1)
             col = random.randint(0, self.cols -1)
             self.mines_set.add((row, col))
+
+        #Create buttons for each cell
+        for row in range(self.rows):
+            for col in range(self.cols):
+                btn = Button(game_area, height = 2, width = 2)
+                btn.grid(row = row, column = col)
+                #Bind left mouse button to show cell. Tkinter callback used for button click
+                btn.bind("<Button-1>", self.reveal_cell_callback(row, col))
+                #Bind right mouse button to show cell. Tkinter callback used for button click
+                btn.bind("<Button-3>", self.flag_cell_callback(row, col))
+                self.buttons[row][col] = btn
+
+    #Define method to retun callback function for revealing cell
+    def reveal_cell_callback(self, row, col):
+        #Callback function that will be called
+        def callback(event):
+            #Call reveal_cell with the row and column
+            self.reveal_cell(row, col)
+        return callback
+
+    #Define method to return callback function for flaggin cell
+    def flag_cell_callback(self, row, col):
+        #Callback function that will be called
+        def callback(event):
+            #Call flag_cell with the row and column
+            self.flag_cell(row, col)
+        return callback
 
 #Run the Game
 root.mainloop()
