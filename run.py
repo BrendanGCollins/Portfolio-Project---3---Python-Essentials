5585509c5c6a753258570d28f410e41811578b15
BrendanGCollins <163342759+BrendanGCollins@users.noreply.github.com>
2024-06-16 11:09:37 +0000

    Add top/side frame

--- run.py
+++ run.py
@@ -11,3 +11,26 @@ root.geometry("600x600")
 root.title("Minesweeper Game")
 #Prevent game area changing outside given height x width
 root.resizable(False, False)
+
+#Add top frame dimensions and color
+top_frame = Frame(
+    root, bg = "grey",
+    width = 600,
+    height = 120,
+)
+
+#Add top frame position
+top_frame.place(x=0, y=0)
+
+#Add right frame dimensions and color
+right_frame = Frame(
+    root, bg = "grey",
+    width = 120,
+    height = 480,
+)
+
+#Add right frame position
+right_frame.place(x=480, y=120)
+
+#Run the Game
+root.mainloop()
